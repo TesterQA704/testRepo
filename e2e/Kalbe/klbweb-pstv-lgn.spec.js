c825c9969bf1b847e37de2cf135a3a4cbbf53536
unknown <wilbur.haratua21@gmail.com>
2024-10-07 15:35:15 +0700

    Update

--- e2e/Kalbe/klbweb-pstv-lgn.spec.js
+++ e2e/Kalbe/klbweb-pstv-lgn.spec.js
@@ -4,8 +4,8 @@ test('amon positive login', async ({ page }) => {
     
     //Login
     await page.goto('https://amon.kalbe.co.id/Lgn');
-    await page.getByLabel('firstname.lastname@compdomain').fill('haidar.wintoro@kalbe.co.id');
-    await page.getByLabel('Password').fill('Kalbefarm4');
+    await page.getByLabel('firstname.lastname@compdomain').fill('faiz.syukri@kalbe.co.id');
+    await page.getByLabel('Password').fill('Kalbefarma2024!');
     await page.getByRole('button', { name: ' Login' }).click();
 
     //Verify Homepage Heading Text
@@ -21,15 +21,14 @@ test('amon positive login', async ({ page }) => {
     const currentURL = page.url();
     expect(currentURL).toBe('https://amon.kalbe.co.id/Lgn');
     
-
 });
 
 test('bsCorp positive login', async ({ page }) => {
     
     //Login
     await page.goto('https://bscorp.kalbe.co.id/');
-    await page.getByPlaceholder('firstname.lastname@compdomain').fill('haidar.wintoro@kalbe.co.id');
-    await page.getByPlaceholder('Password').fill('Kalbefarm4');
+    await page.getByPlaceholder('firstname.lastname@compdomain').fill('faiz.syukri@kalbe.co.id');
+    await page.getByPlaceholder('Password').fill('Kalbefarma2024!');
     await page.getByRole('button', { name: 'Log in' }).click();
     
     //Verify
@@ -50,8 +49,8 @@ test('ils positive login', async ({ page }) => {
     
     //Login
     await page.goto('https://ils.kalbe.co.id/');
-    await page.getByPlaceholder('Your Username').fill('haidar.wintoro@kalbe.co.id');
-    await page.getByPlaceholder('Your Password').fill('Kalbefarm4');
+    await page.getByPlaceholder('Your Username').fill('faiz.syukri@kalbe.co.id');
+    await page.getByPlaceholder('Your Password').fill('Kalbefarma2024!');
     await page.getByRole('button', { name: ' LOG IN' }).click();
     
     //Verify
@@ -73,8 +72,8 @@ test('kation positive login', async ({ page }) => {
     
     //Login
     await page.goto('https://innovation.kalbe.co.id/');
-    await page.getByPlaceholder('user.domain@kalbe.co.id').fill('haidar.wintoro@kalbe.co.id');
-    await page.getByPlaceholder('Password User Domain Onekalbe').fill('Kalbefarm4');
+    await page.getByPlaceholder('user.domain@kalbe.co.id').fill('faiz.syukri@kalbe.co.id');
+    await page.getByPlaceholder('Password User Domain Onekalbe').fill('Kalbefarma2024!');
     await page.getByRole('button', { name: 'Login' }).click();
     
     //Verify
@@ -99,8 +98,8 @@ test('momonline positive login', async ({ page }) => {
     
     //Login
     await page.goto('https://momonline.kalbe.co.id/');
-    await page.getByPlaceholder('login menggunakan user').fill('haidar.wintoro@kalbe.co.id');
-    await page.getByPlaceholder('password').fill('Kalbefarm4');
+    await page.getByPlaceholder('login menggunakan user').fill('faiz.syukri@kalbe.co.id');
+    await page.getByPlaceholder('password').fill('Kalbefarma2024!');
     await page.getByRole('button', { name: '   Login' }).click();
     
     //Verify
@@ -121,8 +120,8 @@ test('PLM positive login', async ({ page }) => {
     
     //Login
     await page.goto('https://plm.kalbe.co.id/');
-    await page.getByPlaceholder('ex: user.plm@kalbe.co.id').fill('budisanto.tanoto@kalbe.co.id');
-    await page.getByPlaceholder('Enter your password').fill('Kalbefarm4');
+    await page.getByPlaceholder('ex: user.plm@kalbe.co.id').fill('faiz.syukri@kalbe.co.id');
+    await page.getByPlaceholder('Enter your password').fill('Kalbefarma2024!');
     await page.getByRole('button', { name: 'LOGIN' }).click();
     
     //Verify
@@ -136,4 +135,4 @@ test('PLM positive login', async ({ page }) => {
     expect(currentURL).toBe('https://plm.kalbe.co.id/Account/Login');
 
 
-});
+});  
